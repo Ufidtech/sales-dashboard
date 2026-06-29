bd6e3fc5bae365fafb7e7220e73e330c83b0d659
Ibrahim  Danjuma <ufidfarooq@gmail.com>
2026-06-29 02:25:44 -0700

    feat: create modern hero, navbar, and footer with tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Components/Footer";
+import Hero from "./Components/Hero"
+import Welcome from "./Components/Welcome";
+import "./App.css";
+
+function MyApp() {
+  return (
+    <>
+      <Welcome />
+      <Hero />
+      <Footer />
+    
+    </>
+  );
+}
+export default MyApp;
